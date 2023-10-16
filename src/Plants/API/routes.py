de14dd1e1efe34d1b34b0022b1658847b358b3ef
Gabriel <acostagabriel1311@gmai.com>
2023-10-16 15:46:03 -0300

    created endpoint to get a specified plant_info

--- src/Plants/API/routes.py
+++ src/Plants/API/routes.py
@@ -1,10 +1,21 @@
 from ninja import Router
-from src.Plants.schemas import Plants_info_Out
+from src.Plants.schemas.plants_info_schemas import PlantsInfoOutput
 from src.Plants.models import Plants_info
+from django.shortcuts import get_object_or_404
+from typing import Any, Dict
 
 router = Router()
 
-@router.get("", response=list[Plants_info_Out])
+@router.get("", response=list[PlantsInfoOutput])
 def plants_info(request):
     plants_info = Plants_info.objects.get_list()
     return plants_info
+
+
+@router.get("/{plants_info_id}", response={
+    200: PlantsInfoOutput,
+    404: Any,
+    500: Dict,
+})
+def get_plant_info_id(request, plants_info_id: int):
+    return get_object_or_404(Plants_info, id=plants_info_id)
--- src/Plants/schemas/plants_info_schemas.py
+++ src/Plants/schemas/plants_info_schemas.py
@@ -1,8 +1,15 @@
 from ninja import Schema
 from pydantic import UUID4
 
-class Plants_info_Out(Schema):
+class PlantsInfoOutput(Schema):
     id: UUID4
+    scientific_name: str
+    station: str
+    temperature: int
+    light: int
+    watering: int
+
+class PlantsInfoInput(Schema):
     scientific_name: str
     station: str
     temperature: int
