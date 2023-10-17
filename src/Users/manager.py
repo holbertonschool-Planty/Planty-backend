2355575e90bad7d383c97e56cd5f3164f0af7353
Faqu22 <facundoalvarez415@gmail.com>
2023-10-16 23:40:49 -0300

    Implementation of token users to save the profile

--- src/Users/manager.py
+++ src/Users/manager.py
@@ -1,22 +1,34 @@
-from django.db.models.manager import Manager
 from src.schemas.schemas import CustomBadRequest
-from utils.models_loads import get_users_model
+from utils.models_loads import get_usersToken_model
 from django.shortcuts import get_object_or_404
+from django.contrib.auth import authenticate
 from uuid import UUID
 from src.schemas import users_schemas
+from django.contrib.auth.models import BaseUserManager
 
-class UsersManager(Manager):
 
-    def create_user(self, data: dict):
-        user_obj = self.create(**data)
+class UsersManager(BaseUserManager):
+
+    def create_user(self, **data):
+        if not data["email"] or not data["password"]:
+            raise ValueError('The credentials must be set')
+        data["email"] = self.normalize_email(data["email"])
+        user = self.model(**data)
+        user.set_password(data["password"])
+        user.save(using=self._db)
+        return user
+
+    def register_user(self, data: dict):
+        user_obj = self.create_user(**data)
         return 201, self.create_schema(user_obj)
-
+   
     def login_user(self, data: users_schemas.UserLogin):
-        user_obj = get_object_or_404(self.model, email=data.email)
-        if user_obj.password == data.password:
-            return 200, self.create_schema(user_obj)
+        user = authenticate(email=data.email, password=data.password)
+        if user:
+            user_token, _ = get_usersToken_model().objects.get_or_create(user=user)
+            return 200, self.create_schema(user, user_token.token)
         else:
-            return CustomBadRequest("Email doest not exist.")
+            raise CustomBadRequest("Invalid credentials")
 
     def update_user(self, users_id:UUID, data: users_schemas.UserInput):
         user_obj = get_object_or_404(self.model, id=users_id)
@@ -29,8 +41,9 @@ class UsersManager(Manager):
         user_obj.delete()
         return 200, {"message": "User deleted succesfully"}
 
-    def create_schema(self, user_obj: users_schemas.UserInput):
+    def create_schema(self, user_obj: users_schemas.UserInput, user_token= None):
         return users_schemas.UserOutput(
             id=user_obj.id,
             name=user_obj.name,
-            email=user_obj.email)
+            email=user_obj.email,
+            token=user_token)
