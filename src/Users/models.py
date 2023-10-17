921a946f00190c50fa362ddd3ce53418918fae52
Faqu22 <facundoalvarez415@gmail.com>
2023-10-16 23:41:04 -0300

    Implementation of token users to save the profile

--- src/Users/models.py
+++ src/Users/models.py
@@ -1,14 +1,17 @@
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 from src.Users.manager import UsersManager
 import uuid
 
-class Users(models.Model):
+class Users(AbstractBaseUser):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     name = models.CharField(max_length=40)
     email = models.EmailField(max_length=100, unique=True)
-    password = models.CharField(max_length=40)
+    password = models.CharField(max_length=128)
     objects = UsersManager()
+    USERNAME_FIELD = 'email'
 
-    class Meta:
-        verbose_name= 'User'
-        verbose_name_plural= 'Users'
+class UserToken(models.Model):
+    user = models.OneToOneField(Users, on_delete=models.CASCADE)
+    token = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
