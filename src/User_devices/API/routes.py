81f3b17bffee2141491c6c72649ad9a0e5d01a81
Faqu22 <facundoalvarez415@gmail.com>
2023-10-24 15:03:50 -0300

    Implementation of update and delete endpoint

--- src/User_devices/API/routes.py
+++ src/User_devices/API/routes.py
@@ -14,7 +14,7 @@ router = Router(tags=["Planty of users"])
 
 
 @router.get(
-    "",
+    "{users_id}",
     response={
     200: List[UserPlantyOutput],
     400: schemas.BadRequestResponse,
@@ -26,7 +26,7 @@ def get_planties_by_user(request, users_id: UUID):
 
 
 @router.get(
-    "{planty_id}",
+    "{users_id}/planty/{planty_id}",
     response={
     200: UserPlantyOutput,
     400: schemas.BadRequestResponse,
@@ -38,7 +38,7 @@ def get_planty_of_user(request, users_id: UUID, planty_id: UUID):
 
 
 @router.post(
-    "{planty_id}",
+    "{users_id}/planty/{planty_id}",
     response={
     201: CreationPlantyUserOutput,
     400: schemas.BadRequestResponse,
@@ -55,16 +55,34 @@ def add_planty_by_user(request, users_id: UUID, planty_id: UUID, data: CreationP
         response_notifications = get_phoneEvent_model().objects.create_events(data.phone_event, userPhone_obj)
     return 201, CreationPlantyUserOutput(user_planty=response_userplanty, phone_events=response_notifications)
 
-
 @router.put(
-    "{planty_id}",
+    "{user_planty_id}/",
     response={
-    201: CreationPlantyUserOutput,
+    200: UserPlantyOutput,
     400: schemas.BadRequestResponse,
     404: schemas.NotFoundResponse,
     500: schemas.InternalServerErrorResponse
     }
 )
-def update_planty_by_user(request, users_id: UUID, planty_id: UUID, data: CreationPlantyUserInput, file: UploadedFile = None):
-    print("AAAAAAAAAAAAAAAAAAAAAAAAaa")
-    return get_userPlanty_model().objects.update_planty_user(users_id, planty_id, data.user_planty, file, data.plants_info_id)
+def update_planty_by_user(request, user_planty_id: UUID, data: CreationPlantyUserInput = None, file: UploadedFile = None):
+    kwargs = {'user_planty_id': user_planty_id}
+    if data and data.user_planty:
+        kwargs['data'] = data.user_planty
+    if data and data.plants_info_id:
+        kwargs['plant_info_id'] = data.plants_info_id
+    if file:
+        kwargs['file'] = file
+    return get_userPlanty_model().objects.update_planty_user(**kwargs)
+    
+
+@router.delete(
+    "{user_planty_id}/",
+    response={
+    200: Dict,
+    400: schemas.BadRequestResponse,
+    404: schemas.NotFoundResponse,
+    500: schemas.InternalServerErrorResponse
+    }
+)
+def delete_planty_by_user(request, user_planty_id:UUID):
+    return get_userPlanty_model().objects.delete_planty_user(user_planty_id)
